9ed313ec6b8b226be7f8431808ba549540180191
Alexandre Zagame <zagamealexandre@gmail.com>
2021-02-25 18:02:13 +0100

    adding functionalities

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Grid } from '@material-ui/core';
-import Details from './components/Details/Details';
+
+import { SpeechState, useSpeechContext } from '@speechly/react-client';
 import {
   PushToTalkButton,
   PushToTalkButtonContainer,
-  BigTranscript,
-  BigTranscriptContainer,
-  ErrorPanel,
 } from '@speechly/react-ui';
 
-import useStyles from './styles';
+// import { Details, Main } from './components';
 import Main from './components/Main/Main';
+import Details from './components/Details/Details';
+import useStyles from './styles';
 
-function App() {
+const App = () => {
   const classes = useStyles();
+  const { speechState } = useSpeechContext();
+  const main = useRef(null);
+
+  const executeScroll = () => main.current.scrollIntoView();
+
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
 
   return (
     <div>
@@ -25,26 +35,24 @@ function App() {
         justify="center"
         style={{ height: '100vh' }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
           <Details title="Expense" />
         </Grid>
+        <PushToTalkButtonContainer>
+          <PushToTalkButton />
+        </PushToTalkButtonContainer>
       </Grid>
-      <BigTranscriptContainer>
-        <BigTranscript />
-      </BigTranscriptContainer>
-
-      <PushToTalkButtonContainer>
-        <PushToTalkButton />
-        <ErrorPanel />
-      </PushToTalkButtonContainer>
     </div>
   );
-}
+};
 
 export default App;
